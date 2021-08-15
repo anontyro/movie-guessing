4a3f6293ae4a4d8e5cc823c37ae084abb4fceafd
Alexander Wilkinson <anontyro@users.noreply.github.com>
2021-08-15 11:24:35 +0800

    added in filtered for the movies section updated styles

--- client/src/pages/Home/components/Movies/AllResults.tsx
+++ client/src/pages/Home/components/Movies/AllResults.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MovieItem from '../../../../interfaces/MovieItem';
 import {
   Button,
@@ -12,14 +12,18 @@ import {
   Statistic,
   Grid,
   Accordion,
+  Input,
+  Dropdown,
+  DropdownProps,
 } from 'semantic-ui-react';
 import styled from '@emotion/styled';
 import BaseCard from '../../../../components/Cards/BaseCard';
+import { getMostRecentDecades } from '../../../../utils/maths';
 
 const MainResultContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  max-height: 50vh;
+  height: 50vh;
   overflow-y: auto;
   justify-content: center;
 `;
@@ -28,6 +32,131 @@ const ResultsMetaContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
+
+const MovieFilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+const MovieFilterRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  & > * {
+    margin-right: 10px;
+  }
+  & > *:last-child {
+    margin-right: 0;
+  }
+
+  @media (max-width: 600px) {
+    & > * {
+      margin-right: 0;
+      margin-bottom: 10px;
+      width: 100%;
+    }
+    & > *:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+interface MovieFilterProps {
+  movies: MovieItem[];
+  setFilteredMovies: (movies: MovieItem[]) => void;
+}
+
+const MovieFilter: React.FC<MovieFilterProps> = ({
+  movies,
+  setFilteredMovies,
+}) => {
+  const [query, setQuery] = useState('');
+
+  const getStringQueryFilteredMovies = (val: string) => {
+    const query = val.toLowerCase();
+    const filtered = movies.filter((m) =>
+      m.name.toLocaleLowerCase().includes(query),
+    );
+
+    return filtered;
+  };
+
+  const getMovieDecadeFilteredMovies = (val: number, movies: MovieItem[]) => {
+    const filtered = movies.filter(
+      (m) => m.releaseYear >= val && m.releaseYear <= val + 10,
+    );
+
+    return filtered;
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setQuery(value);
+    if (value.trim() === '' || value === null) {
+      setFilteredMovies(movies);
+      return;
+    }
+    const filtered = getStringQueryFilteredMovies(value);
+    setFilteredMovies(filtered);
+  };
+
+  const recentDecades = getMostRecentDecades();
+  const recentDecadesOptions = recentDecades.map((d, i) => ({
+    key: i + 1,
+    text: `${d}`,
+    value: d,
+  }));
+
+  const [selectedDecade, setSelectedDecade] = useState<{
+    key: number;
+    text: string;
+    value: number;
+  }>({
+    key: 0,
+    text: '',
+    value: 0,
+  });
+  const onDecadeChange = (event: any, data: DropdownProps) => {
+    console.log(event.target.textContent);
+    const value = event.target.textContent ?? '';
+    const key = parseInt(value, 10) | 0;
+    setSelectedDecade({
+      key: key,
+      text: value,
+      value: key,
+    });
+    const filtered = getStringQueryFilteredMovies(query);
+    if (key !== 0) {
+      const filteredByDecade = getMovieDecadeFilteredMovies(key, filtered);
+      setFilteredMovies(filteredByDecade);
+    } else {
+      setFilteredMovies(filtered);
+    }
+  };
+
+  return (
+    <MovieFilterContainer>
+      <MovieFilterRow>
+        <Input
+          icon="search"
+          placeholder="Find Movie..."
+          value={query}
+          onChange={onChange}
+        />
+        <Dropdown
+          placeholder="Select Decade"
+          options={recentDecadesOptions}
+          clearable
+          selection
+          onChange={onDecadeChange}
+          value={selectedDecade.value}
+        />
+      </MovieFilterRow>
+    </MovieFilterContainer>
+  );
+};
 
 interface Props {
   movies: MovieItem[];
@@ -51,6 +180,9 @@ const AllResults: React.FC<Props> = ({
     const data = event.dataTransfer.getData('text/plain');
     console.log(`drop here with data`, data);
   };
+
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
   return (
     <Accordion className="m-bot-1" fluid styled>
       <Accordion.Title active={isActive} onClick={() => setIsActive(!isActive)}>
@@ -58,8 +190,9 @@ const AllResults: React.FC<Props> = ({
         All Movies
       </Accordion.Title>
       <Accordion.Content active={isActive}>
+        <MovieFilter movies={movies} setFilteredMovies={setFilteredMovies} />
         <MainResultContainer>
-          {movies.map((movie) => (
+          {filteredMovies.map((movie) => (
             <BaseCard key={movie.imdbId} dragStart={onDragStart(movie.imdbId)}>
               <Card.Content>
                 <Card.Header>
--- client/src/utils/maths.ts
+++ client/src/utils/maths.ts
@@ -1,3 +1,13 @@
 export const randomIntFromInterval = (max: number, min = 0) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+export const getMostRecentDecades = () => {
+  const currentYear = new Date().getFullYear();
+  const decades = [];
+  for (let i = currentYear; i >= 1960; i -= 10) {
+    const replaceLast = `${i.toString().substr(0, 3)}0`;
+    decades.push(parseInt(replaceLast));
+  }
+  return decades;
+};
